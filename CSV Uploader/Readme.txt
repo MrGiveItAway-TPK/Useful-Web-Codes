4fe8ac708baecf29ff0c317b796dec5f095baf62
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-03-14 11:36:29 +0300

    Delete CSV Uploader directory

--- CSV Uploader/Readme.txt
+++
@@ -1,3 +0,0 @@
-1.Put Normally in localhost.
-2.create a database with the name of uploader.
-3.import sql tables and just run the code and test it out.
--- CSV Uploader/php/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost","root","","uploader");
-
-if ($conn -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $conn -> connect_error;
-  exit();
-}
-?>
